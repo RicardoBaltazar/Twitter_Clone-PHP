691ca1deff6c2609f4fea1886c0797f637cf141a
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-16 20:29:08 -0300

    respondendo requisições com base na url requisitada

--- aula1/server.js
+++ aula1/server.js
@@ -1,7 +1,15 @@
 let http = require('http')
 
 let server = http.createServer(function(req, res){
-    res.end('<html><body>Ola mundo"</body></html>')
+    let categoria = req.url
+
+    if(categoria == '/tecnologia'){
+        res.end('<html><body>tecnologia</body></html>')
+    } else if (categoria == '/moda') {
+        res.end('<html><body>moda</body></html>')
+    } else {
+        res.end('<html><body>Ola mundo</body></html>')
+    }
 })
 
 server.listen(8000)
