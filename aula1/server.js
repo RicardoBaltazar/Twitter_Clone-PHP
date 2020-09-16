e416c84ec089b6470d1525a107e8cc6632e62d45
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-16 20:20:24 -0300

    respondendo requisições http com node

--- aula1/server.js
+++ aula1/server.js
@@ -1,1 +1,7 @@
-console.log('Ola mundo!')
+let http = require('http')
+
+let server = http.createServer(function(req, res){
+    res.end('<html><body>Ola mundo"</body></html>')
+})
+
+server.listen(8000)
