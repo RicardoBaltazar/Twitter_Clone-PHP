e6f2740acf9efb5c19ceeba53487a0133b3f5b74
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-17 20:10:01 -0300

    criando conexão com mysql e recuperando dados

--- aula1/app/routes/noticias.js
+++ aula1/app/routes/noticias.js
@@ -1,5 +1,23 @@
-module.exports = function(app){
-    app.get('/noticias', function(req, res){
-        res.render('noticias/noticias')
+module.exports = function (app) {
+
+    app.get('/noticias', function (req, res) {
+
+        let mysql = require('mysql')
+
+        //estabelecer a conexão com o banco de dados
+        let connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            passowrd: '26260',
+            database: 'portal_noticias' //nome do banco de dados criado no mysql server
+        })
+
+        //realizando as consultas
+        connection.query('select * from noticias', function(error, result) {
+            res.send(result)
+            console.log(result)
+        })
+
+        //res.render('noticias/noticias')
     })
 }
