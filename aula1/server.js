f5ea028bc4456650c2df3a0141edef89e6a00876
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-17 01:48:18 -0300

    refactoring do projeto

--- aula1/server.js
+++ aula1/server.js
@@ -4,15 +4,15 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
-    res.send('Ola mundo!')
+    res.render('home/index')
 })
 
-app.get('/tecnologia', function(req, res){
-    res.render('secao/tecnologia')
+app.get('/form_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia')
 })
 
-app.get('/moda', function(req, res){
-    res.send('moda')
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias')
 })
 
 app.listen(8000, function(){
