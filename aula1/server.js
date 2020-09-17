f2b8458f7dd3317c1d8bc6a8ce39da8a3e99edac
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-16 21:05:03 -0300

    instalando ejs

--- aula1/server.js
+++ aula1/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res){
-    res.send('Ola Mundo!!')
+    res.send('Ola mundo!')
+})
+
+app.get('/tecnologia', function(req, res){
+    res.render('secao/tecnologia')
+})
+
+app.get('/moda', function(req, res){
+    res.send('moda')
 })
 
 app.listen(8000, function(){
@@ -12,5 +22,6 @@ app.listen(8000, function(){
 
 /*
  npm install express -save -> baixar o express para dentro do projeto
+ npm install ejs -save
 
 */
