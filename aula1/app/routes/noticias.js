63a869d39ed2ce5929675496215c3f10e3bc5d3f
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-18 12:37:16 -0300

    atualizando versoes das dependencias

--- aula1/app/routes/noticias.js
+++ aula1/app/routes/noticias.js
@@ -1,3 +1,4 @@
+
 module.exports = function (app) {
 
     app.get('/noticias', function (req, res) {
@@ -21,3 +22,34 @@ module.exports = function (app) {
         //res.render('noticias/noticias')
     })
 }
+
+/*
+const mysql = require('mysql');
+const { promisify } = require('util');
+
+const listar = async () => {
+  const conexao = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '26260',
+    database : 'portal_noticias'
+  });
+
+  const query = promisify(conexao.query);
+
+  return query('select * from noticias');
+};
+
+module.exports = (app) => {
+  app.get('/noticias', async (req, res) => {
+    try {
+      res.send(await listar());
+    } catch(e) {
+      console.error(e);
+      res.status(500).send(e.message);
+    }
+  });
+};
+
+
+*/
